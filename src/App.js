5da62ed3ee5ba392f72603de065db40200eb2468
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2021-03-22 21:44:08 +0200

    додає навiгацiю

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 
-class App extends Component {
-  state = {};
+import { Route, NavLink, Switch } from 'react-router-dom';
+import HomeView from './views/HomeView';
+import AuthorsView from './views/AuthorsView';
+import BooksView from './views/BooksView';
+import NotFoundView from './views/NotFoundView';
 
-  render() {
-    return <></>;
-  }
-}
+// class App extends Component {
+//   state = {};
+
+//   render() {
+//     return <></>;
+//   }
+// }
+
+const App = () => (
+  <>
+    <ul>
+      <li>
+        <NavLink
+          exact
+          to="/"
+          className="NavLink"
+          activeClassName="NavLink--active"
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/authors"
+          className="NavLink"
+          activeClassName="NavLink--active"
+        >
+          Authors
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/books"
+          className="NavLink"
+          activeClassName="NavLink--active"
+        >
+          Books
+        </NavLink>
+      </li>
+    </ul>
+
+    <Switch>
+      <Route exact path="/" component={HomeView} />
+      <Route path="/authors" component={AuthorsView} />
+      <Route path="/books" component={BooksView} />
+      <Route component={NotFoundView} />
+    </Switch>
+  </>
+);
 
 export default App;
